bb681114c7dc2c527454e12bc8771b805235993a
Anonymous <none@none.none>
2023-03-05 22:49:34 +0100

    fetch.sh: Do not hardcode path, give feedback

--- fetch.sh
+++ fetch.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-OWLDIR=/home/fdov/owl/ukraine/map
-OWLFILE=$OWLDIR/$(date '+%y%m%d_%H%M').kmz
-cd $OWLDIR
-wget -q -O $OWLFILE https://www.google.com/maps/d/u/0/kml?mid=180u1IkUjtjpdJWnIC0AxTKSiqK4G6Pez
-git checkout master
-git add $OWLFILE
-git commit -m "Daily fetch." 
-git push
+FOLDER=$1
+EXPORT_URL='https://www.google.com/maps/d/u/0/kml?mid=180u1IkUjtjpdJWnIC0AxTKSiqK4G6Pez'
+SAVE_TO=${FOLDER:-.}/$(date '+%y%m%d_%H%M').kmz
+BRANCH=master
+REMOTE=origin
+echo "Saving to ${SAVE_TO}"
+wget -q -O ${SAVE_TO} ${EXPORT_URL}
+git checkout ${BRANCH}
+git add $SAVE_TO
+git commit -m "Daily fetch: $(date '+%y%m%d_%H%M')"
+git push ${REMOTE} ${BRANCH}
