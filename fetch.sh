9482c1bad78e871ad0bb48d86cf6703520912fbc
ix5 <ix5@users.noreply.github.com>
2023-03-06 20:25:00 +0100

    Add periodic update action, cleanup fetch.sh (#1)
    * Update fetch script to push to git.
    
    * fetch.sh: Do not hardcode path, give feedback
    
    * .github: Add periodic update action
    
    * Move git push logic to push.sh
    
    * workflows: Change cron time to less crowded hours
    
    ---------
    
    Co-authored-by: fdov <fdov21@gmail.com>
    Co-authored-by: ix5 <none@none.none>

--- fetch.sh
+++ fetch.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-OWLDIR=/home/fdov/owl/ukraine/map
-wget -q -O $OWLDIR/$(date '+%y%m%d_%H%M').kmz https://www.google.com/maps/d/u/0/kml?mid=180u1IkUjtjpdJWnIC0AxTKSiqK4G6Pez
+FOLDER=$1
+EXPORT_URL='https://www.google.com/maps/d/u/0/kml?mid=180u1IkUjtjpdJWnIC0AxTKSiqK4G6Pez'
+SAVE_TO=${FOLDER:-.}/$(date '+%y%m%d_%H%M').kmz
+echo "Saving to ${SAVE_TO}"
+wget -q -O ${SAVE_TO} ${EXPORT_URL}
