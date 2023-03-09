0a4ed335a02f475854bbc5a73d7b000476a02746
fdov <fdov21@gmail.com>
2023-03-09 18:55:51 +0100

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 BRANCH=master
 REMOTE=origin
+FOLDER=$1
+EXPORT_URL='https://www.google.com/maps/d/u/0/kml?mid=180u1IkUjtjpdJWnIC0AxTKSiqK4G6Pez'
+SAVE_TO=${FOLDER:-.}/$(date '+%y%m%d_%H%M').kmz
 git checkout ${BRANCH}
 git add $SAVE_TO
 git commit -m "Daily fetch: $(date '+%y%m%d_%H%M')"
