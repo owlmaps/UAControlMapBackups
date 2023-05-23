c4dff515b3df5f606629b92d994c39d76680d1d0
fdov21 <126599750+fdov21@users.noreply.github.com>
2023-05-23 09:21:36 +0200

    Update fetch.sh
    Replace the referer

--- fetch.sh
+++ fetch.sh
@@ -3,4 +3,4 @@ FOLDER=$1
 EXPORT_URL='https://www.google.com/maps/d/u/0/kml?mid=180u1IkUjtjpdJWnIC0AxTKSiqK4G6Pez'
 SAVE_TO=${FOLDER:-.}/$(date '+%y%m%d_%H%M').kmz
 echo "Saving to ${SAVE_TO}"
-wget --referer='https://owlunits.com' --retry-connrefused -q -O ${SAVE_TO} ${EXPORT_URL}
+wget --referer='https://www.google.com/maps/d/u/0/viewer?mid=180u1IkUjtjpdJWnIC0AxTKSiqK4G6Pez&ll=48.42522162072407%2C33.18003022257546&z=7' --retry-connrefused -O ${SAVE_TO} ${EXPORT_URL}
