69adb68f17b6074dbf69b3e2432c86d149d10124
fdov21 <126599750+fdov21@users.noreply.github.com>
2023-05-23 09:19:31 +0200

    Update fetch.sh
    Retry if connection is refused

--- fetch.sh
+++ fetch.sh
@@ -3,4 +3,4 @@ FOLDER=$1
 EXPORT_URL='https://www.google.com/maps/d/u/0/kml?mid=180u1IkUjtjpdJWnIC0AxTKSiqK4G6Pez'
 SAVE_TO=${FOLDER:-.}/$(date '+%y%m%d_%H%M').kmz
 echo "Saving to ${SAVE_TO}"
-wget --referer='https://owlunits.com' -q -O ${SAVE_TO} ${EXPORT_URL}
+wget --referer='https://owlunits.com' --retry-connrefused -q -O ${SAVE_TO} ${EXPORT_URL}
