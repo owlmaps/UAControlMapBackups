4d050ed639c05e21c95574e1a1ae5a01f9744d05
fdov <fdov21@gmail.com>
2023-05-27 20:06:44 +0200

    Retry download for a while.

--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,25 @@
-#!/bin/sh
+#!/bin/bash
 FOLDER=$1
 EXPORT_URL='https://www.google.com/maps/d/u/0/kml?mid=180u1IkUjtjpdJWnIC0AxTKSiqK4G6Pez'
 SAVE_TO=${FOLDER:-.}/$(date '+%y%m%d_%H%M').kmz
-echo "Saving to ${SAVE_TO}"
-wget --referer='https://www.google.com/maps/d/u/0/viewer?mid=180u1IkUjtjpdJWnIC0AxTKSiqK4G6Pez&ll=48.42522162072407%2C33.18003022257546&z=7' --retry-connrefused -O ${SAVE_TO} ${EXPORT_URL}
+tries=30
+retry_secs=30
+
+fetchloop() {
+rt=0
+    while [ $rt -lt $tries ]; do
+        if wget -O ${SAVE_TO} ${EXPORT_URL}; then
+            echo "Saved to ${SAVE_TO}"
+        return 0
+	  else
+        echo "We don't give up that easily... Retrying in $retry_secs s..."
+		sleep $retry_secs
+        let "rt+=1"
+    fi
+    done
+
+    echo "FAIL: $rt retries... didn't work. Shit."
+    return 1
+}
+
+fetchloop
