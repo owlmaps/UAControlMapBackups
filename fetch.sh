eb89ec8ea1e6aa4935cdc1e21e3f876fba79c16a
fdov21 <126599750+fdov21@users.noreply.github.com>
2023-06-02 14:08:02 +0200

    Update fetch.sh
    More retries.

--- fetch.sh
+++ fetch.sh
@@ -2,7 +2,7 @@
 FOLDER=$1
 EXPORT_URL='https://www.google.com/maps/d/u/0/kml?mid=180u1IkUjtjpdJWnIC0AxTKSiqK4G6Pez'
 SAVE_TO=${FOLDER:-.}/$(date '+%y%m%d_%H%M').kmz
-tries=30
+tries=90
 retry_secs=30
 
 fetchloop() {
@@ -12,9 +12,10 @@ rt=0
             echo "Saved to ${SAVE_TO}"
         return 0
 	  else
-        echo "We don't give up that easily... Retrying in $retry_secs s..."
+        echo -n "We don't give up that easily... Retrying in $retry_secs s..."
 		sleep $retry_secs
         let "rt+=1"
+	echo "($rt)"
     fi
     done
 
