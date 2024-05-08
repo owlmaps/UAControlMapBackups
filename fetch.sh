5487503cee0f7d85e4772fb75af7db35ed040c98
fdov21 <fdov21@gmail.com>
2024-05-08 08:38:04 +0200

    More tries

--- fetch.sh
+++ fetch.sh
@@ -2,8 +2,8 @@
 FOLDER=$1
 EXPORT_URL='https://www.google.com/maps/d/u/0/kml?mid=180u1IkUjtjpdJWnIC0AxTKSiqK4G6Pez'
 SAVE_TO=${FOLDER:-.}/$(date '+%y%m%d_%H%M').kmz
-tries=90
-retry_secs=30
+tries=200
+retry_secs=40
 
 fetchloop() {
 rt=0
