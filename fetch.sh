6c5d9d0dfa4a2bdc1c55f2bd3a12348358bdd466
fdov21 <fdov21@gmail.com>
2024-05-10 08:58:35 +0200

    Manual update

--- fetch.sh
+++ fetch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 FOLDER=$1
-EXPORT_URL='https://www.google.com/maps/d/u/0/kml?mid=180u1IkUjtjpdJWnIC0AxTKSiqK4G6Pez'
+EXPORT_URL='https://www.google.com/maps/d/kml?mid=180u1IkUjtjpdJWnIC0AxTKSiqK4G6Pez'
 SAVE_TO=${FOLDER:-.}/$(date '+%y%m%d_%H%M').kmz
-tries=200
-retry_secs=40
+tries=90
+retry_secs=30
 
 fetchloop() {
 rt=0
